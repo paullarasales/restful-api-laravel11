67d9fea83ea97d3fb36cce7231249e4272ae9a0c
paullarasales <ahmadzufukli@gmail.com>
2024-08-23 09:04:16 +0800

    done with the request

--- app/Http/Requests/StoreProductRequest.php
+++ app/Http/Requests/StoreProductRequest.php
@@ -30,6 +30,10 @@ class StoreProductRequest extends FormRequest
     }
 
     public function failedValidation(Validator $validator) {
-        
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Validation errors',
+            'data' => $validator->errors()
+        ]));
     }
 }
