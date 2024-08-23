24566bef984ad549e53972a8bdcc22e7cf689d9f
paullarasales <ahmadzufukli@gmail.com>
2024-08-23 08:50:15 +0800

    restful-api laravel 11

---
+++ app/Repositories/ProductRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Repository;
+use App\Models\Product;
+use App\Interfaces\ProductRepositoryInterface;
+class ProductReposiotry implements ProductRepositoryInterface
+{
+    public function index(){
+        return Product::all();
+    }
+
+    public function getById($id){
+       return Product::findOrFail($id);
+    }
+
+    public function store(array $data){
+       return Product::create($data);
+    }
+
+    public function update(array $data,$id){
+       return Product::whereId($id)->update($data);
+    }
+    
+    public function delete($id){
+       Product::destroy($id);
+    }
+}
