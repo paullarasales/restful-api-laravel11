e19ae283d21937c5fbd257d3f4846a27957a37b1
paullarasales <ahmadzufukli@gmail.com>
2024-08-24 22:44:14 +0800

    finished validation

--- app/Http/Requests/StoreStudentRequest.php
+++ app/Http/Requests/StoreStudentRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
 
 class StoreStudentRequest extends FormRequest
 {
@@ -11,7 +13,7 @@ class StoreStudentRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +24,19 @@ class StoreStudentRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'firstname' => 'required',
+            'middlename' => 'required',
+            'lastname' => 'required',
+            'age' => 'required'
         ];
+    }
+
+    public function failedValidation(Validator $validator) 
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Validation errors',
+            'data' => $validator->errors()
+        ]));
     }
 }
