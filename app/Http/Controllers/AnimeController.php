4aa219f9fefd9e1e5b557591d51ec11d3480e0f9
paullarasales <ahmadzufukli@gmail.com>
2024-08-25 10:51:45 +0800

    creating anime api

---
+++ app/Http/Controllers/AnimeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreAnimeRequest;
+use App\Http\Requests\UpdateAnimeRequest;
+use App\Models\Anime;
+
+class AnimeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAnimeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Anime $anime)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Anime $anime)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAnimeRequest $request, Anime $anime)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Anime $anime)
+    {
+        //
+    }
+}
