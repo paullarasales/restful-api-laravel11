bcd3a4e61f6283e112e7ebf2acf13251ad5fb2eb
paullarasales <ahmadzufukli@gmail.com>
2024-08-25 11:04:53 +0800

    done creating repo for anime

---
+++ app/Repositories/AnimeRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Anime;
+use App\Interfaces\AnimeRepositoryInterface;
+
+class AnimeRepository implements AnimeRepositoryInterface
+{
+    public function index()
+    {
+        return Anime::limit(4)->get();
+    }
+
+    public function getById($id) 
+    {
+        return Anime::findOrFail($id);
+    }
+
+    public function store(array $data) 
+    {
+        return Anime::create($data);
+    }
+
+    public function update(array $data,$id) 
+    {
+        return Anime::whereID($id)->update($data);
+    }
+
+    public function delete($id)
+    {
+        Anime::destroy($id);
+    }
+}
