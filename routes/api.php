22d078f3938727babecddcb1772ead5fbd47f7a3
paullarasales <ahmadzufukli@gmail.com>
2024-08-25 20:41:53 +0800

    finished creating api route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\AnimeController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,3 +12,4 @@ Route::get('/user', function (Request $request) {
 
 Route::apiResource('/products',ProductController::class);
 Route::apiResource('/students',StudentController::class);
+Route::apiResource('/anime',AnimeController::class);
